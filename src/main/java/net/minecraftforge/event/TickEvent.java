a29922c9da64257e376ae859689ecd77209f0cd8
Paint_Ninja <PaintNinja@users.noreply.github.com>
2025-09-20 12:20:46 +0100

    Remove deprecated functionality from TickEvent

--- src/main/java/net/minecraftforge/event/TickEvent.java
+++ src/main/java/net/minecraftforge/event/TickEvent.java
@@ -14,53 +14,22 @@ import java.util.function.BooleanSupplier;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.eventbus.api.bus.EventBus;
-import net.minecraftforge.eventbus.api.event.InheritableEvent;
 import net.minecraftforge.eventbus.api.event.MutableEvent;
 import net.minecraftforge.fml.LogicalSide;
 
-public sealed class TickEvent extends MutableEvent implements InheritableEvent {
-    /**
-     * @deprecated Use one of the subclasses instead, as subscribing to the abstract {@link TickEvent} class will call
-     * your listener possibly hundreds of times per tick which is inefficient/slow and probably not what you want
-     */
-    @Deprecated(forRemoval = true, since = "1.21.6")
-    public static final EventBus<TickEvent> BUS = EventBus.create(TickEvent.class);
-
-    public enum Type {
-        LEVEL, PLAYER, CLIENT, SERVER, RENDER
-    }
-
-    public enum Phase {
-        START, END
-    }
-
-    public final Type type;
+public sealed abstract class TickEvent extends MutableEvent {
     public final LogicalSide side;
 
-    /**
-     * @deprecated For better performance, add listeners to the Pre or Post event subclasses instead of checking the phase
-     */
-    @Deprecated(forRemoval = true, since = "1.21.6")
-    public final Phase phase;
-
-    public TickEvent(Type type, LogicalSide side, Phase phase) {
-        this.type = type;
+    protected TickEvent(LogicalSide side) {
         this.side = side;
-        this.phase = phase;
     }
 
-    public static sealed class ServerTickEvent extends TickEvent {
-        /**
-         * @deprecated Use {@link ServerTickEvent.Pre} or {@link ServerTickEvent.Post} instead
-         */
-        @Deprecated(forRemoval = true, since = "1.21.6")
-        public static final EventBus<ServerTickEvent> BUS = EventBus.create(ServerTickEvent.class);
-
+    public static sealed abstract class ServerTickEvent extends TickEvent {
         private final BooleanSupplier haveTime;
         private final MinecraftServer server;
 
-        protected ServerTickEvent(BooleanSupplier haveTime, MinecraftServer server, Phase phase) {
-            super(Type.SERVER, LogicalSide.SERVER, phase);
+        protected ServerTickEvent(BooleanSupplier haveTime, MinecraftServer server) {
+            super(LogicalSide.SERVER);
             this.haveTime = haveTime;
             this.server = server;
         }
@@ -85,7 +54,7 @@ public sealed class TickEvent extends MutableEvent implements InheritableEvent {
             public static final EventBus<Pre> BUS = EventBus.create(Pre.class);
 
             public Pre(BooleanSupplier haveTime, MinecraftServer server) {
-                super(haveTime, server, Phase.START);
+                super(haveTime, server);
             }
         }
 
@@ -93,53 +62,33 @@ public sealed class TickEvent extends MutableEvent implements InheritableEvent {
             public static final EventBus<Post> BUS = EventBus.create(Post.class);
 
             public Post(BooleanSupplier haveTime, MinecraftServer server) {
-                super(haveTime, server, Phase.END);
+                super(haveTime, server);
             }
         }
     }
 
-    public static sealed class ClientTickEvent extends TickEvent {
-        /**
-         * @deprecated Use {@link ClientTickEvent.Pre} or {@link ClientTickEvent.Post} instead
-         */
-        @Deprecated(forRemoval = true, since = "1.21.6")
-        public static final EventBus<ClientTickEvent> BUS = EventBus.create(ClientTickEvent.class);
-
-        protected ClientTickEvent(Phase phase) {
-            super(Type.CLIENT, LogicalSide.CLIENT, phase);
+    public static sealed abstract class ClientTickEvent extends TickEvent {
+        protected ClientTickEvent() {
+            super(LogicalSide.CLIENT);
         }
 
         public static final class Pre extends ClientTickEvent {
             public static final EventBus<Pre> BUS = EventBus.create(Pre.class);
             public static final Pre INSTANCE = new Pre();
-
-            public Pre() {
-                super(Phase.START);
-            }
         }
 
         public static final class Post extends ClientTickEvent {
             public static final EventBus<Post> BUS = EventBus.create(Post.class);
             public static final Post INSTANCE = new Post();
-
-            public Post() {
-                super(Phase.END);
-            }
         }
     }
 
-    public static sealed class LevelTickEvent extends TickEvent {
-        /**
-         * @deprecated Use {@link LevelTickEvent.Pre} or {@link LevelTickEvent.Post} instead
-         */
-        @Deprecated(forRemoval = true, since = "1.21.6")
-        public static final EventBus<LevelTickEvent> BUS = EventBus.create(LevelTickEvent.class);
-
+    public static sealed abstract class LevelTickEvent extends TickEvent {
         public final Level level;
         private final BooleanSupplier haveTime;
 
-        protected LevelTickEvent(LogicalSide side, Level level, BooleanSupplier haveTime, Phase phase) {
-            super(Type.LEVEL, side, phase);
+        protected LevelTickEvent(LogicalSide side, Level level, BooleanSupplier haveTime) {
+            super(side);
             this.level = level;
             this.haveTime = haveTime;
         }
@@ -158,7 +107,7 @@ public sealed class TickEvent extends MutableEvent implements InheritableEvent {
             public static final EventBus<Pre> BUS = EventBus.create(Pre.class);
 
             public Pre(LogicalSide side, Level level, BooleanSupplier haveTime) {
-                super(side, level, haveTime, Phase.START);
+                super(side, level, haveTime);
             }
         }
 
@@ -166,22 +115,16 @@ public sealed class TickEvent extends MutableEvent implements InheritableEvent {
             public static final EventBus<Post> BUS = EventBus.create(Post.class);
 
             public Post(LogicalSide side, Level level, BooleanSupplier haveTime) {
-                super(side, level, haveTime, Phase.END);
+                super(side, level, haveTime);
             }
         }
     }
 
-    public static sealed class PlayerTickEvent extends TickEvent {
-        /**
-         * @deprecated Use {@link PlayerTickEvent.Pre} or {@link PlayerTickEvent.Post} instead
-         */
-        @Deprecated(forRemoval = true, since = "1.21.6")
-        public static final EventBus<PlayerTickEvent> BUS = EventBus.create(PlayerTickEvent.class);
-
+    public static sealed abstract class PlayerTickEvent extends TickEvent {
         public final Player player;
 
-        protected PlayerTickEvent(Player player, Phase phase) {
-            super(Type.PLAYER, player instanceof ServerPlayer ? LogicalSide.SERVER : LogicalSide.CLIENT, phase);
+        protected PlayerTickEvent(Player player) {
+            super(player instanceof ServerPlayer ? LogicalSide.SERVER : LogicalSide.CLIENT);
             this.player = player;
         }
 
@@ -189,7 +132,7 @@ public sealed class TickEvent extends MutableEvent implements InheritableEvent {
             public static final EventBus<Pre> BUS = EventBus.create(Pre.class);
 
             public Pre(Player player) {
-                super(player, Phase.START);
+                super(player);
             }
         }
 
@@ -197,22 +140,16 @@ public sealed class TickEvent extends MutableEvent implements InheritableEvent {
             public static final EventBus<Post> BUS = EventBus.create(Post.class);
 
             public Post(Player player) {
-                super(player, Phase.END);
+                super(player);
             }
         }
     }
 
     public static sealed abstract class RenderTickEvent extends TickEvent {
-        /**
-         * @deprecated Use {@link RenderTickEvent.Pre} or {@link RenderTickEvent.Post} instead
-         */
-        @Deprecated(forRemoval = true, since = "1.21.6")
-        public static final EventBus<RenderTickEvent> BUS = EventBus.create(RenderTickEvent.class);
-
         private final DeltaTracker timer;
 
-        private RenderTickEvent(Phase phase, DeltaTracker timer) {
-            super(Type.RENDER, LogicalSide.CLIENT, phase);
+        private RenderTickEvent(DeltaTracker timer) {
+            super(LogicalSide.CLIENT);
             this.timer = timer;
         }
 
@@ -224,7 +161,7 @@ public sealed class TickEvent extends MutableEvent implements InheritableEvent {
             public static final EventBus<Pre> BUS = EventBus.create(Pre.class);
 
             public Pre(DeltaTracker timer) {
-                super(Phase.START, timer);
+                super(timer);
             }
         }
 
@@ -232,7 +169,7 @@ public sealed class TickEvent extends MutableEvent implements InheritableEvent {
             public static final EventBus<Post> BUS = EventBus.create(Post.class);
 
             public Post(DeltaTracker timer) {
-                super(Phase.END, timer);
+                super(timer);
             }
         }
 
