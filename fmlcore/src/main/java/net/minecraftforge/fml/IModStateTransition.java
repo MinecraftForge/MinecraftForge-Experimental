fee94342cacd0326b03f2c1467d98a19c11a937f
Paint_Ninja <PaintNinja@users.noreply.github.com>
2026-03-21 00:08:41 +0000

    Remove deprecated IModStateTransition#eventFunctionStream

--- fmlcore/src/main/java/net/minecraftforge/fml/IModStateTransition.java
+++ fmlcore/src/main/java/net/minecraftforge/fml/IModStateTransition.java
@@ -11,16 +11,11 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.function.BiFunction;
 import java.util.function.Function;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
 
 import net.minecraftforge.fml.loading.progress.ProgressMeter;
 import org.jetbrains.annotations.Nullable;
 
 public interface IModStateTransition {
-    /** Magic value to allow me to optimize the futures list by ignoring the default value without making old methods nullable. */
-    BiFunction<Executor, ? extends EventGenerator<?>, CompletableFuture<Void>> NULL_HOOK = (_, _) -> CompletableFuture.completedFuture(null);
-
     static IModStateTransition buildNoopTransition() {
         return ModStateTransitionHelper.NOOP;
     }
@@ -38,15 +33,6 @@ public interface IModStateTransition {
 
     default BiFunction<ModLoadingStage, Throwable, ModLoadingStage> nextModLoadingStage() {
         return ModLoadingStage::nextState;
-    }
-
-    /**
-     * This used to allow you to fire multiple events during the transition. However, in doing so it would cause issues with the default
-     * ModContainer's event handlers causing issues such as mod classes being constructed multiple times.
-     */
-    @Deprecated(since = "1.21.3", forRemoval = true)
-    default Supplier<Stream<EventGenerator<?>>> eventFunctionStream() {
-        return () -> Stream.ofNullable(eventFunction());
     }
 
     @Nullable
